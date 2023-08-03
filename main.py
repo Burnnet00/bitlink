7a80dec33036250c30107c650620efe13d32bbe0
Burnnet <Burnnet00@gmail.com>
2023-08-03 16:36:06 +0300

    hide token

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import json
 import requests
 from urllib.parse import urlparse
+from dotenv import load_dotenv
+import os
 
-token = "Bearer a649aa3e3ce528d1d3f42b594cbff90731a0d0ba"
+
+load_dotenv()
+token = os.environ['TOKEN']
 
 
 def shorten_link(token, url):
@@ -17,25 +21,25 @@ def shorten_link(token, url):
         "Content-Type": "application/json"
     }
 
-    url = 'https://api-ssl.bitly.com/v4/shorten'
+    url = "https://api-ssl.bitly.com/v4/shorten"
     response = requests.post(url, json=payload, headers=headers)
     response.raise_for_status()
     bitlink = response.json()['link']
     return bitlink
 
 
-print('Enter your link: ')
+print("Enter your link: ")
 try:
     user_link = input()
     url = user_link
     bitlink = shorten_link(token, url)
-    print('Битлинк -', bitlink)
+    print("Битлинк -", bitlink)
 except requests.exceptions.HTTPError:
-    print('Bad link')
+    print("Bad link")
 
 
 def count_clicks(token, link):
-    url = f'https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks/summary'
+    url = f"https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks/summary"
     headers = {
         "Authorization": token,
     }
@@ -45,19 +49,20 @@ def count_clicks(token, link):
     return clicks_count
 
 
-print('Enter short link: ')
+print("Enter short link: ")
 try:
     user_short_link = input()
     parsed = urlparse(user_short_link)
     link = parsed.netloc + parsed.path
     link_count = count_clicks(token, link)
     dict_count = json.loads(link_count)["total_clicks"]
-    print('our link has been followed', dict_count, 'times')
+    print("our link has been followed", dict_count, "times")
 except requests.exceptions.HTTPError:
-    print('Bad link')
+    print("Bad link")
 
-# def is_bitlink(url)
-
+def is_bitlink(url):
+    url = f"https: // api - ssl.bitly.com / v4 / bitlinks / {url}"
+    return
 
 
 # if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,5 +1,25 @@
-from urllib.parse import urlparse
+# from urllib.parse import urlparse
+#
+# url = 'https://bit.ly/3KuFXqv'
+# parsed = urlparse(url)
+# print(parsed.netloc+parsed.path)
+import requests
 
-url = 'https://bit.ly/3KuFXqv'
-parsed = urlparse(url)
-print(parsed.netloc+parsed.path)
+def is_bitlink(url):
+    url = f"https://api-ssl.bitly.com/v4/bitlinks/{url}"
+    headers = {
+        "Authorization": "Bearer a649aa3e3ce528d1d3f42b594cbff90731a0d0ba",
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    print(response.text)
+a = 'bit.ly/3KuFXqv'
+b = 'pymotw.com/3/urllib.parse/index.html'
+is_bitlink(b)
+
+
+
+# {"created_at":"2023-08-03T11:42:37+0000","id":"bit.ly/3KuFXqv","link":"https://bit.ly/3KuFXqv","custom_bitlinks":[],"long_url":"https://pymotw.com/3/urllib.parse/index.html","title":"urllib.parse — Split URLs into Components — PyMOTW 3","archived":false,"created_by":"burnnet","client_id":"a5e8cebb233c5d07e5c553e917dffb92fec5264d","tags":[],"deeplinks":[],"references":{"group":"https://api-ssl.bitly.com/v4/groups/Bn82bpRqty3"}}
+
+# https://pymotw.com/3/urllib.parse/index.html
+# https://bit.ly/3KuFXqv
